3f75f5efe576dc2a07c8a153f6df01bcaa166071
danibasconCHAKRAY <daniel.bascon@chakray.co.uk>
2018-09-07 08:38:34 +0200

    ssh

--- instances.tf
+++ instances.tf
@@ -21,6 +21,12 @@ resource "aws_security_group" "web" {
      protocol = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+     from_port = 0
+     to_port = 0 
+     protocol = -1
+     cidr_blocks = ["0.0.0.0/0"]
+}
   tags {
      Name = "ssh"
   }
